a1efe0637022e4a4c922eaeaeca00026f42f27d2
Md Iearulislam <md.iearulislam@bauer.de>
2023-12-14 15:45:06 +0100

    Added CQRS pattern in API structure

--- symfony_backend/migrations/Version20231214070810.php
+++ symfony_backend/migrations/Version20231214070810.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231211210447 extends AbstractMigration
+final class Version20231214070810 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,19 +20,14 @@ final class Version20231211210447 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE products_id_seq CASCADE');
         $this->addSql('CREATE TABLE products (id UUID NOT NULL, name VARCHAR(255) NOT NULL, price DOUBLE PRECISION NOT NULL, description TEXT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN products.id IS \'(DC2Type:product_id)\'');
-        $this->addSql('DROP TABLE migration_versions');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('CREATE SEQUENCE products_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE migration_versions (version VARCHAR(14) NOT NULL, executed_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(version))');
-        $this->addSql('COMMENT ON COLUMN migration_versions.executed_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('DROP TABLE products');
     }
 }
--- symfony_backend/src/Controller/ProductsController.php
+++ symfony_backend/src/Controller/ProductsController.php
@@ -2,30 +2,48 @@
 
 namespace App\Controller;
 
-use App\Entity\Products;
+use App\Query\GetAllProductsQuery;
+use App\Query\GetProductByIdQuery;
+use App\QueryHandler\GetAllProductsQueryHandler;
+use App\QueryHandler\GetProductByIdQueryHandler;
+use App\ValueObject\ProductId;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\Persistence\ManagerRegistry;
-
 
 class ProductsController extends AbstractController
 {
+    private $getAllProductsQueryHandler;
+    private $getProductByIdQueryHandler;
+
+    public function __construct(
+        GetAllProductsQueryHandler $getAllProductsQueryHandler,
+        GetProductByIdQueryHandler $getProductByIdQueryHandler
+    ) {
+        $this->getAllProductsQueryHandler = $getAllProductsQueryHandler;
+        $this->getProductByIdQueryHandler = $getProductByIdQueryHandler;
+    }
+
     #[Route('/api/products', name: 'Product List', methods: ['GET'])]
-    public function index(ManagerRegistry $doctrine): JsonResponse
+    public function index(): JsonResponse
     {
-        $products = $doctrine->getRepository(Products::class)->findAll();
+        $query = new GetAllProductsQuery();
+        $data = ($this->getAllProductsQueryHandler)($query);
 
-        $data = [];
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
 
-        foreach ($products as $product) {
-            $data[] = [
-                'id' => $product->getId(),
-                'name' => $product->getName(),
-                'price' => $product->getPrice(),
-                'description' => $product->getDescription(),
-            ];
+
+    #[Route('/api/products/{id}', name: 'Product Detail', methods: ['GET'])]
+    public function show(string $id): JsonResponse
+    {
+        $productId = new ProductId($id);
+        $query = new GetProductByIdQuery($productId);
+        $data = ($this->getProductByIdQueryHandler)($query);
+
+        if (!$data) {
+            return new JsonResponse(['error' => 'Product not found'], Response::HTTP_NOT_FOUND);
         }
 
         return new JsonResponse($data, Response::HTTP_OK);
